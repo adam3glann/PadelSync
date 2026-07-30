b4f3f4c535b8a8b95db0119dec16683d6f0fd773
Adam Aglan <adamadel3224@gmail.com>
2026-07-30 18:09:50 +0300

    Add backend setup and user registration

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({
+        message: "PadelSync Backend is Running 🚀"
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+};
+
+startServer();
